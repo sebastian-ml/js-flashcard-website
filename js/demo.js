526ac31828306c780a6197d3e86857aa656b22c9
sebastian-ml <sebastian2549@wp.pl>
2021-02-16 18:45:21 +0100

    Refactor demo and flashcard code

--- js/demo.js
+++ js/demo.js
@@ -1,97 +1,99 @@
 import { questions, questionsAnswered } from "./flashcard.js";
 import { getRandomNumber } from "./script.js";
 
-const container = document.getElementById('demo-container');
-const nextQuestionBtn = document.getElementById('demo-next-question');
-const cardContainer = document.getElementById('demo-card-container');
+const container = document.getElementById("demo-container");
+const nextQuestionBtn = document.getElementById("demo-next-question");
+const cardContainer = document.getElementById("demo-card-container");
 const htmlToRemove = [
-    document.getElementsByClassName('par par--md')[0],
-    document.getElementsByClassName('center')[0],
+  document.getElementsByClassName("par par--md")[0],
+  document.getElementsByClassName("center")[0],
 ];
 
 let currentQuestionIndex;
 let currentQuestion;
-let userAnswered = false;
+let userAnswer = false;
 
 let checkButton;
 let answersLi;
 let userInput;
 
-container.addEventListener('click', (e) => {
-    const targetID = e.target.id;
+container.addEventListener("click", (e) => {
+  const targetID = e.target.id;
 
-    if (targetID === 'demo-start' || targetID === 'demo-next-question') {
-        if (targetID === 'demo-start') {
-            htmlToRemove.forEach(ele => container.removeChild(ele));
-            cardContainer.style.display = 'flex';
-        } else if (targetID === 'demo-next-question') {
-            nextQuestionBtn.style.visibility = 'hidden';
-            questions.splice(currentQuestionIndex, 1);
-            questionsAnswered.push(currentQuestion);
-        }
-        generateCard(questions.length);
-        getTriggers(currentQuestion.cardType);
-        checkIfAnswered();
-        checkButton.addEventListener('click', () => {
-            showNextBtn(userAnswered, questions.length);
-        })
+  if (targetID === "demo-start" || targetID === "demo-next-question") {
+    if (targetID === "demo-start") {
+      htmlToRemove.forEach((ele) => container.removeChild(ele));
+      cardContainer.style.display = "flex";
+    } else if (targetID === "demo-next-question") {
+      nextQuestionBtn.style.visibility = "hidden";
+      questions.splice(currentQuestionIndex, 1);
+      questionsAnswered.push(currentQuestion);
     }
-})
+    generateCard();
+    getTriggers(currentQuestion.cardType);
+    checkUserAnswer();
+    checkButton.addEventListener("click", () => {
+      showNextBtn(questions.length);
+    });
+  }
+});
 
-function generateCard(cardsLeft) {
-    currentQuestionIndex = getRandomNumber(questions.length);
-    currentQuestion = questions[currentQuestionIndex];
-    const questionCard = currentQuestion.createCard(
-        'demo-card',
-        'demo-check-btn',
-        'demo-answers',
-        'demo-input'
-    );
+function generateCard() {
+  currentQuestionIndex = getRandomNumber(questions.length);
+  currentQuestion = questions[currentQuestionIndex];
+  const questionCard = currentQuestion.createCard(
+    "demo-card",
+    "demo-check-btn",
+    "demo-answers",
+    "demo-input"
+  );
 
-    // Remove card if it exists
-    const activeCard = document.getElementById('demo-card');
-    if (activeCard !== null) {
-        activeCard.remove();
-    }
+  // Remove card if it exists
+  const activeCard = document.getElementById("demo-card");
+  if (activeCard !== null) {
+    activeCard.remove();
+  }
 
-    cardContainer.prepend(questionCard);
+  cardContainer.prepend(questionCard);
 }
 
 function getTriggers(cardType) {
-    checkButton = document.getElementById('demo-check-btn');
+  checkButton = document.getElementById("demo-check-btn");
 
-    if (cardType === 'millionaire') {
-        const answersUl = document.getElementById('demo-answers');
-        answersLi = answersUl.getElementsByTagName('input');
-    } else if (cardType === 'write') {
-        userInput = document.getElementById('demo-input');
-    }
+  if (cardType === "millionaire") {
+    const answersUl = document.getElementById("demo-answers");
+    answersLi = answersUl.getElementsByTagName("input");
+  } else if (cardType === "write") {
+    userInput = document.getElementById("demo-input");
+  }
 }
 
-function checkIfAnswered() {
-    checkButton.addEventListener('click', () => {
-        if (['millionaire', 'write'].includes(currentQuestion.cardType)) {
-            const userAnswer = getUserAnswer(currentQuestion.cardType).trim();
-            userAnswered = userAnswer !== undefined && userAnswer.length > 0;
-        } else {
-            userAnswered = true;
-        }
-    })
-}
-
-function showNextBtn(userAnswered, cardsLeft) {
-    if (userAnswered && cardsLeft > 1) {
-        nextQuestionBtn.style.visibility = 'visible';
-        userAnswered = false;
+function checkUserAnswer() {
+  checkButton.addEventListener("click", () => {
+    if (currentQuestion.cardType === "millionaire") {
+      userAnswer = [...answersLi].filter((answer) => answer.checked === true)[0]
+        .value;
+    } else if (currentQuestion.cardType === "write") {
+      userAnswer = userInput.value.trim();
     } else {
-        console.log('There are no more cards');
+      userAnswer = currentQuestion.answers.filter(
+        (option) => option.correct === true
+      )[0].answer;
     }
+  });
 }
 
-function getUserAnswer(cardType) {
-    if (cardType === 'millionaire') {
-        return [...answersLi].filter(answer => answer.checked === true)[0].value;
-    } else if (cardType === 'write') {
-        return userInput.value;
-    }
+function showNextBtn(cardsLeft) {
+  if (userAnswer !== false && userAnswer.length !== 0 && cardsLeft > 1) {
+    nextQuestionBtn.style.visibility = "visible";
+    userAnswer = false;
+  } else if (
+    userAnswer === false ||
+    userAnswer === undefined ||
+    userAnswer.length === 0
+  ) {
+    console.log("Answer the question!");
+  } else {
+    console.log("There are no more cards");
+  }
 }
--- js/flashcard.js
+++ js/flashcard.js
@@ -1,142 +1,129 @@
 import { createHtmlElement } from "./script.js";
 
 class Flashcard {
-    constructor(cardType, question, answers) {
-        this.cardType = cardType.toLowerCase();
-        this.question = question;
-        this.answers = answers;
+  constructor(cardType, question, answers) {
+    this.cardType = cardType.toLowerCase();
+    this.question = question;
+    this.answers = answers;
 
-        this.box = 1;
+    this.box = 1;
+  }
+
+  createCard(cardID, cardBtnID, cardAnswersID, cardInputID) {
+    const card = createHtmlElement(
+      "div",
+      ["card", "flashcard-questions__card"],
+      cardID
+    );
+    card.dataset.cardType = this.cardType;
+
+    const cardQuestion = createHtmlElement("h3", ["heading", "card__question"]);
+    if (this.cardType === "gap") {
+      cardQuestion.innerText = this.question.replace(
+        this.answers[0]["answer"],
+        "[...]"
+      );
+    } else {
+      cardQuestion.innerText = this.question;
     }
 
-    createCard(cardID, cardBtnID, cardAnswersID, cardInputID) {
-        const card = createHtmlElement('div', ['card', 'flashcard-questions__card'], cardID);
-        card.dataset.cardType = this.cardType;
+    if (["classic", "gap", "write"].some((type) => type === this.cardType)) {
+      const cardBody = createHtmlElement("div", ["card__body"]);
+      cardBody.appendChild(cardQuestion);
 
-        const cardQuestion = createHtmlElement('h3',['heading', 'card__question']);
-        if (this.cardType === 'gap') {
-            cardQuestion.innerText = this.question.replace(this.answers[0]['answer'], '[...]');
-        } else {
-            cardQuestion.innerText = this.question;
-        }
+      if (this.cardType === "write") {
+        const label = createHtmlElement("label");
 
-        if (['classic', 'gap', 'write'].some(type => type === this.cardType)) {
-            const cardBody = createHtmlElement('div',['card__body']);
-            cardBody.appendChild(cardQuestion);
-
-            if (this.cardType === 'write') {
-                const label = createHtmlElement('label');
-
-                const cardInput = createHtmlElement(
-                    'input',
-                    ['card__input'],
-                    cardInputID
-                );
-                cardInput.type = 'text';
-                cardInput.placeholder = 'Enter your answer'
-
-                label.appendChild(cardInput);
-                cardBody.appendChild(label);
-            }
-            card.appendChild(cardBody);
-        }
-
-        if (this.cardType === 'millionaire') {
-            const cardAnswers = createHtmlElement(
-                'ul',
-                ['card__answers'],
-                cardAnswersID
-            );
-
-            this.answers.forEach((answer, index) => {
-                const cardAnswer = createHtmlElement(
-                    'li',
-                    ['card__answer']
-                );
-
-                const cardRadio = createHtmlElement(
-                    'input',
-                    ['card__radio'],
-                    'answer-' + index
-                );
-                cardRadio.type = 'radio';
-                cardRadio.name = 'card-option'
-                cardRadio.value = answer['answer'];
-
-                const cardLabel = createHtmlElement('label',['card__label']);
-                cardLabel.htmlFor = 'answer-' + index;
-                cardLabel.innerText = answer['answer'];
-
-                cardAnswer.appendChild(cardRadio);
-                cardAnswer.appendChild(cardLabel);
-                cardAnswers.appendChild(cardAnswer);
-            })
-
-            card.appendChild(cardQuestion);
-            card.appendChild(cardAnswers);
-        }
-
-        const cardBtn = createHtmlElement(
-            'button',
-            ['btn', 'card__btn'],
-            cardBtnID
+        const cardInput = createHtmlElement(
+          "input",
+          ["card__input"],
+          cardInputID
         );
-        cardBtn.innerText = 'Check your answer';
-        card.appendChild(cardBtn);
+        cardInput.type = "text";
+        cardInput.placeholder = "Enter your answer";
 
-        return card;
+        label.appendChild(cardInput);
+        cardBody.appendChild(label);
+      }
+      card.appendChild(cardBody);
     }
 
-    checkAnswer(userAnswer) {
-        const correctAnswer =
-            (this.answers).filter(answer => answer['correct'])[0]['answer'];
-        let isCorrect = false;
+    if (this.cardType === "millionaire") {
+      const cardAnswers = createHtmlElement(
+        "ul",
+        ["card__answers"],
+        cardAnswersID
+      );
 
-        if (userAnswer === correctAnswer) {
-            this.box++;
-            isCorrect = true;
-        } else {
-            if (this.box !== 1) this.box--;
-        }
+      this.answers.forEach((answer, index) => {
+        const cardAnswer = createHtmlElement("li", ["card__answer"]);
 
-        return isCorrect;
+        const cardRadio = createHtmlElement(
+          "input",
+          ["card__radio"],
+          "answer-" + index
+        );
+        cardRadio.type = "radio";
+        cardRadio.name = "card-option";
+        cardRadio.value = answer["answer"];
+
+        const cardLabel = createHtmlElement("label", ["card__label"]);
+        cardLabel.htmlFor = "answer-" + index;
+        cardLabel.innerText = answer["answer"];
+
+        cardAnswer.appendChild(cardRadio);
+        cardAnswer.appendChild(cardLabel);
+        cardAnswers.appendChild(cardAnswer);
+      });
+
+      card.appendChild(cardQuestion);
+      card.appendChild(cardAnswers);
     }
+
+    const cardBtn = createHtmlElement(
+      "button",
+      ["btn", "card__btn"],
+      cardBtnID
+    );
+    cardBtn.innerText = "Check your answer";
+    card.appendChild(cardBtn);
+
+    return card;
+  }
+
+  checkAnswer(userAnswer) {
+    const correctAnswer = this.answers.filter((answer) => answer["correct"])[0][
+      "answer"
+    ];
+
+    this.changeBox(correctAnswer === userAnswer);
+    return correctAnswer === correctAnswer;
+  }
+
+  changeBox(isAnswerCorrect) {
+    if (this.box > 1 && this.box < 5) {
+      isAnswerCorrect ? this.box++ : (this.box = 1);
+    }
+  }
 }
 
-export const questions = [];
 export const questionsAnswered = [];
-
-questions[0] = new Flashcard(
-    'millionaire',
-    'What is the highest mountain in the world?',
-    [
-        {answer: 'Mont Blanc', correct: false},
-        {answer: 'Mount Everest', correct: true},
-        {answer: 'Nanga Parbat', correct: false},
-        {answer: 'Kilimanjaro', correct: false},
-    ]
-);
-
-questions[1] = new Flashcard(
-    'gap',
-    'Nile is the longest river in the world.',
-    [
-        {answer: 'Nile', correct: true},
-    ]
-);
-
-questions[2] = new Flashcard(
-    'classic',
-    'What is the capital city of Finland?',
-    [
-        {answer: 'Helsinki', correct: true},
-    ]
-);
-
-questions[3] = new Flashcard(
-    'write',
-    'Who won the US Presidential election in 2020? Name and surname is required.',
-    [
-        {answer: 'Joe Biden', correct: true},
-    ]
-);
+export const questions = [
+  new Flashcard("millionaire", "What is the highest mountain in the world?", [
+    { answer: "Mont Blanc", correct: false },
+    { answer: "Mount Everest", correct: true },
+    { answer: "Nanga Parbat", correct: false },
+    { answer: "Kilimanjaro", correct: false },
+  ]),
+  new Flashcard("gap", "Nile is the longest river in the world.", [
+    { answer: "Nile", correct: true },
+  ]),
+  new Flashcard("classic", "What is the capital city of Finland?", [
+    { answer: "Helsinki", correct: true },
+  ]),
+  new Flashcard(
+    "write",
+    "Who won the US Presidential election in 2020? Name and surname is required.",
+    [{ answer: "Joe Biden", correct: true }]
+  ),
+];
